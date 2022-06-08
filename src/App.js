00f468768b52dad80a503e85ecd4b707efcac5bd
Canberk <canberkkaya06@gmail.com>
2022-06-08 16:11:49 +0300

    first initialization

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react'
+import './App.css'
+import { API } from 'aws-amplify'
 
 function App() {
+  const [apiData, setApiData] = useState('')
+
+  const handleClick = async () => {
+    const data = await API.get('dev-noteapp', '/note/list', {
+      headers: {},
+    })
+    console.log(data)
+    setApiData(data.body)
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className='App'>
+      <header className='App-header'>
+        <button onClick={handleClick}>Click me!</button>
       </header>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
