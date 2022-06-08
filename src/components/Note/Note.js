393f1a99f2094c9cc6b12c0f8da667558d64896d
Canberk <canberkkaya06@gmail.com>
2022-06-08 18:26:18 +0300

    fix delete icon click issue

--- src/components/Note/Note.js
+++ src/components/Note/Note.js
@@ -23,17 +23,17 @@ export default function Notes({ data }) {
   }
 
   return (
-    <Link to={`/notes/${data.noteId}`}>
-      <div
-        className='relative block w-full border-2 border-gray-300 border-dashed rounded-lg p-12 text-center hover:border-gray-400 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500'
-        key={data.notesId}>
-        <TrashIcon
-          onClick={handleDeleteNote}
-          className='flex-shrink-0 h-5 w-5 text-green-500 left-max top-auto'
-          aria-hidden='true'
-        />
+    <div
+      className='relative block w-full border-2 border-gray-300 border-dashed rounded-lg p-12 text-center hover:border-gray-400 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500'
+      key={data.notesId}>
+      <TrashIcon
+        onClick={handleDeleteNote}
+        className='flex-shrink-0 h-5 w-5 text-green-500 left-max top-auto'
+        aria-hidden='true'
+      />
+      <Link to={`/notes/${data.noteId}`}>
         <h3> {data.noteMessage} </h3>
-      </div>
-    </Link>
+      </Link>
+    </div>
   )
 }
